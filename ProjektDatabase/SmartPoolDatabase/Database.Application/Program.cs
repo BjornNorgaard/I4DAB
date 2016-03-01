48bea0006086df406550841ca3aa2f6236a1421e
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-03-01 20:10:38 +0100

    made some console app, throws a ton of exceptions...

---
+++ ProjektDatabase/SmartPoolDatabase/Database.Application/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartPoolDatabase;
+
+namespace Database.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseAccess databaseAccess = new DatabaseAccess();
+            User Nors = new User("Nors", "derp@gmail.com", "motdepasse");
+
+            databaseAccess.InsertUserIntoDatabase(Nors);
+            databaseAccess.PrintUsersInDatabase();
+        }
+    }
+}
---
+++ ProjektDatabase/SmartPoolDatabase/SmartPoolDatabase/SmartPoolDatabase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SmartPoolDatabase
+{
+    public class SmartPoolDatabase : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            Name = "John Smith";
+            Email = "fuckyou@thatswhy.com";
+            Password = "password123";
+        }
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+
+    public class DatabaseAccess
+    {
+        /// <summary>
+        /// Inserts User into SmartPoolDatabase.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>Return 0 on success.</returns>
+        public int InsertUserIntoDatabase(User user)
+        {
+            using (var db = new SmartPoolDatabase())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+
+            return 0;
+        }
+
+        public void PrintUsersInDatabase()
+        {
+            using (var db = new SmartPoolDatabase())
+            {
+                var query = from u in db.Users
+                            orderby u.Name
+                            select u;
+
+                Console.WriteLine("**** USERS IN DATABASE *****");
+                foreach (User user in query)
+                {
+                    Console.WriteLine("\t" + user.Name);
+                }
+            }
+        }
+    }
+}
