1d058b45ead3ca8de55466ee3c6f1d39c05944df
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-05-12 22:05:38 +0200

    not working

---
+++ Handin4/Handin4DB.Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Handin;
+
+namespace Handin4DB.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISensorAccess _sensorAccess = new SensorAccess();
+
+            _sensorAccess.AddData(1, 2, 3, "4");
+        }
+    }
+}
