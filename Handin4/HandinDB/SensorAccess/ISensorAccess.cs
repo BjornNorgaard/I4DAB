7bb364f1c8e538bcacc60766b274d6fa839dd1b4
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-05-12 13:55:31 +0200

    added interface

--- Handin4/HandinDB/SensorAccess/ISensorAccess.cs
+++ Handin4/HandinDB/SensorAccess/ISensorAccess.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +7,6 @@ namespace Handin
 {
     public interface ISensorAccess
     {
-        void method()
+        bool AddData(int sensorId, int appartmentId, double value, string timestamp);
     }
 }
