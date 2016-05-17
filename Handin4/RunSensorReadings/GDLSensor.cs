d77e7e9fb4a37a31e5d9c1d8118d682a0a649d78
Norgaard Laptop <Bjornsnorgaard@gmail.com>
2016-05-17 12:19:11 +0200

    shit works!

--- Handin4/RunSensorReadings/GDLSensor.cs
+++ Handin4/RunSensorReadings/GDLSensor.cs
@@ -6,6 +6,7 @@
         public int AppartmentId { get; set; }
         public double Value { get; set; }
         public string Timestamp { get; set; }
+
         public GDLSensor(int sensorId, int appartmentId, double value, string timestamp)
         {
             SensorId = sensorId;
